e538702d452acb615af1ce357a9280945202e3ec
RoastChestnut <30802664+GES233@users.noreply.github.com>
2026-02-05 20:06:27 +0800

    Refactor layout handling in LayoutBuilder module
    Refactor layout processing to separate layout and write process. Update run function to handle input more effectively.

--- lib/greenhouse/steps/layout_builder.ex
+++ lib/greenhouse/steps/layout_builder.ex
@@ -1,15 +1,26 @@
 defmodule Greenhouse.Steps.LayoutBuilder do
+  aliases Greenhouse.Params.{Post, Page, Link}
   use Orchid.Step
 
   def as_declarative(_opts \\ []), do: [
-    # Seperate layout and write
-    {__MODULE__, [:posts_map_with_doc_struct, :generated_posts_entrance], :post_router_content_pair},
-    {__MODULE__, [:pages_map_with_doc_struct, :generated_pages_entrance], :page_router_content_pair},
+    # Seperate layout and write process.
+    {__MODULE__, :posts_map_with_doc_struct, :post_router_content_pair},
+    {__MODULE__, :pages_map_with_doc_struct, :page_router_content_pair},
   ]
 
-  def run([_map_with_doc_struct, _target_entrance], _step_options) do
-    {:ok, Orchid.Param.new(:any, :router_content_pair)}
+  def run(map_with_doc_struct, _step_options) do
+    {:ok,
+     Orchid.Param.get_payload(map_with_doc_struct)
+     |> Task.async_stream(&add_layout/1)
+     |> Enum.map(fn {:ok, r} -> r end)
+     |> then(&Orchid.Param.new(:any, :router_content_pair， &1))}
   end
 
-  # def add_layout(page_or_post) do
+  def add_layout(%Post{} = post) do
+    {Link.convert(post), ""}
+  end
+
+  def add_layout(%Page{} = page) do
+    {Link.convert(page), ""}
+  end
 end
