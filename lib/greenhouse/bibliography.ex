7205619793a027f1ff255d591196e0b517b2fdee
RoastedChestnut <chestnut.roasted@outlook.com>
2026-02-09 10:58:30 +0800

    feat(bib): add GB/T 7714-2015 CSL styles and documentation
    - Added CSL files for GB/T 7714-2015 numeric and author-date formats in `priv/csl`.
    - Updated `Greenhouse.Bibliography` with documentation explaining the support for different citation standards based on academic fields and regions.
    - Added TODOs for implementing dynamic CSL loading via tags or categories.

--- lib/greenhouse/bibliography.ex
+++ lib/greenhouse/bibliography.ex
@@ -1,9 +1,25 @@
 defmodule Greenhouse.Bibliography do
+  @moduledoc """
+  提供参考文献的支持。
+
+  不同的主体采用不同的格式，默认国标（数字序号）。
+
+  * 国内偏理工农领域（e.g. 毕设） => GB（数字）
+  * 国内社科文献较多/社科范式下针对国内的研究对象的讨论 => GB（作者年份）
+  # TODO: APA/MLA/IEEE etc.(csl file and scope)
+
+  这里的「国内」指中国大陆，如果以港台相关的文献/对象为主则采用对应地区的推荐格式。
+  """
+
   def to_get_pandoc_meta(%{title: title, extra: extra}, bib_entry) do
     with pandoc_options when map_size(pandoc_options) > 0 <- Map.get(extra, "pandoc", %{}),
          {:ok, bib_path_realtive} <- Map.fetch(pandoc_options, "bibliography"),
          bib_path = Path.join([bib_entry, bib_path_realtive]),
          true <- File.exists?(bib_path) do
+      # TODO: Load csl via tag or categories
+      # Get csl file via
+      # https://www.zotero.org/styles?q=...
+
       %{
         "bibliography" => bib_path,
         "csl" => Map.get(pandoc_options, "csl") || "GB7714",
