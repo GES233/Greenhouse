e90845ded9ff2962e7e7325ce698675755552839
RoastChestnut <chestnut.roasted@outlook.com>
2026-02-14 15:19:29 +0800

    add some code

--- lib/greenhouse/layout/view.ex
+++ lib/greenhouse/layout/view.ex
@@ -2,4 +2,14 @@ defmodule Greenhouse.Layout.View do
   require EEx
 
   EEx.function_from_file(:def, :scaffold, "priv/layout/scaffold.html.eex", [:assigns])
+
+  EEx.function_from_file(:def, :meta, "priv/layout/meta.html.eex", [:assigns])
+
+  # EEx.function_from_file(:def, :prose, ...)
+
+  # EEx.function_from_file(:def, :posts, ...)
+
+  # EEx.function_from_file(:def, :pages, ...)
+
+  # EEx.function_from_file(:def, :taxonomy, ...)
 end
